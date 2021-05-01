758a0e128407ab5f0204eac7c3214b6271d1372f
Alessandra Gutierrez <alessandragutierrez98@gmail.com>
2021-05-01 14:42:17 -0700

    Completed meme-gallery.

---
+++ main.js
@@ -0,0 +1,28 @@
+var imageURLs = [
+  'https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://etcanada.com/wp-content/uploads/2016/06/58c0500b2ac4c342c6fc4ab096ada150.jpg?quality=80&strip=all&crop=0px%2C21px%2C500px%2C333px&resize=720%2C480',
+  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg'
+];
+var gallery = document.querySelector('main');
+var imageURLInput = document.querySelector('input');
+var addImageButton = document.querySelector('button');
+
+addImageButton.addEventListener('click', function (addImage) {
+  if (imageURLInput.value != '') {
+    imageURLs.push(imageURLInput.value);
+  }
+  imageURLInput.value = ''
+  updateGallery();
+});
+
+function updateGallery() {
+  gallery.innerHTML = ''
+  for(i = 0; i < imageURLs.length; i++) {
+    var imageElement = document.createElement('img');
+    imageElement.classList.add('gallery-image');
+    imageElement.src = (imageURLs[i]);
+    gallery.appendChild(imageElement);
+  }
+}
+
+updateGallery();
